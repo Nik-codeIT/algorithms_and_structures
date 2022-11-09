810aeb70b1f664df704c961667283b69e3568ed3
Nik <115698529+Nik-codeIT@users.noreply.github.com>
2022-11-09 17:12:29 +0300

    add graphs algorithms

---
+++ breadth_search.js
@@ -0,0 +1,33 @@
+// Graph
+// FIFO
+
+const graph = {}
+graph.a = ['b', 'c']
+graph.b = ['f']
+graph.c = ['d', 'e']
+graph.d = ['f']
+graph.e = ['f']
+graph.f = ['g']
+
+count = 0;
+
+function breadthSearch(graph, start, end) {
+    let queue = []
+    queue.push(start)
+    while (queue.length > 0) {
+        count++
+        const current = queue.shift()
+        if (!graph[current]) {
+            graph[current] = []
+        }
+        if (graph[current].includes(end)) {
+            return true
+        } else {
+            queue = [...queue, ...graph[current]]
+        }
+    }
+    return false
+}
+
+console.log(breadthSearch(graph, 'a', 'b'))
+console.log(`Количество итераций: ${count}`)
